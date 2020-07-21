9135212ae4cb3c524c2e0d763d0571b00960910a
songseon <68282265+songseon@users.noreply.github.com>
2020-07-21 20:53:03 +0900

    Update add-nbo.c

--- add-nbo.c
+++ add-nbo.c
@@ -1,39 +1,37 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <arpa/inet.h>
+int cross_file(char *message, const char* mode)
+{
+	uint32_t a;
+	uint32_t *pa = &a;
+	uint32_t cross;
+
+	FILE *fp = fopen(message, mode);
+	fread(pa, 1, 4, fp);
+	assert(pa != 0);
+	cross = ntohl(a);
+
+	fclose(fp);
+
+	return cross;	
+}
 
 int main(int argc, char *argv[])
 {   
-    uint32_t a;
-    uint32_t *pa = &a;
-    uint32_t cross_a;
-
-    uint32_t b;
-    uint32_t *pb = &b;
-    uint32_t cross_b;
-
-    uint32_t sum_cross;
+	uint32_t a1, b1;
+    uint32_t sum_cross=0;
     
     if(argc < 3)
     {
-        printf("Usage : %s 1.bin 2.bin", argv[0]);
+        printf("Usage : %s thousand.bin five-hundred.bin", argv[0]);
         return 0;
     }
     
-    FILE *f1 = fopen(argv[1], "rb");
-    fread(pa, 4, 1, f1);
-    cross_a = ntohl(a);
-    
-    FILE *f2 = fopen(argv[2], "rb");
-    fread(pb, 4, 1, f2);
-    cross_b = ntohl(b);
+    a1 = cross_file(argv[1], "rb");
+    sum_cross += a1;
 
-    sum_cross = cross_a + cross_b;
+	b1 = cross_file(argv[2], "rb");
+	sum_cross += b1;
     
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", cross_a, cross_a, cross_b, cross_b, sum_cross, sum_cross);
-
-    fclose(f1);
-    fclose(f2);
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", a1, a1, b1, b1, sum_cross, sum_cross);
 
     return 0;
 }
